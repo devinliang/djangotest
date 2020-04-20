f86fbdcf88ac4a5546766a42b9204e636febae91
devinliang <devinliang@gmail.com>
2020-04-20 19:46:11 +0800

    建立基本測試檔

---
+++ djangotest/urls.py
@@ -0,0 +1,28 @@
+"""djangotest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+from catalog.views import sayhello, hello, hello3, hello4
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', sayhello),
+    url(r'^$', sayhello),
+    url(r'^hello/(\w+)/$', hello),
+    url(r'^hello3/(\w+)/$', hello3),
+    url(r'^hello4/(\w+)/$', hello4),
+]
